293e78888c09593347e1a4dafed5ba0b02553f9a
Daniel Chesters <daniel.chesters@gmail.com>
2016-07-27 23:31:21 +0200

    Refactor unit tests

--- src/test/java/net/danielchesters/dicenotation/test/DiceTests.java
+++ src/test/java/net/danielchesters/dicenotation/test/DiceTests.java
@@ -1,6 +1,7 @@
 package net.danielchesters.dicenotation.test;
 
 import net.danielchesters.dicenotation.DiceNotation;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.allOf;
@@ -14,44 +15,37 @@ public class DiceTests {
 
     @Test
     public void testD6() {
-        assertThat(DiceNotation.roll("d6"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(6)));
+        assertThat(DiceNotation.roll("d6"), getExpectedValue(1, 6));
     }
 
     @Test
     public void testD4() {
-        assertThat(DiceNotation.roll("d4"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(4)));
+        assertThat(DiceNotation.roll("d4"), getExpectedValue(1, 4));
     }
 
     @Test
     public void testD8() {
-        assertThat(DiceNotation.roll("d8"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(8)));
+        assertThat(DiceNotation.roll("d8"), getExpectedValue(1, 8));
     }
 
     @Test
     public void testD10() {
-        assertThat(DiceNotation.roll("d10"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(10)));
+        assertThat(DiceNotation.roll("d10"), getExpectedValue(1, 10));
     }
 
     @Test
     public void testD12() {
-        assertThat(DiceNotation.roll("d12"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(12)));
+        assertThat(DiceNotation.roll("d12"), getExpectedValue(1, 12));
     }
 
     @Test
     public void testD20() {
-        assertThat(DiceNotation.roll("d20"), allOf(greaterThanOrEqualTo(1),
-            lessThanOrEqualTo(20)));
+        assertThat(DiceNotation.roll("d20"), getExpectedValue(1, 20));
     }
 
     @Test
     public void test2D6() {
-        assertThat(DiceNotation.roll("2d6"), allOf(greaterThanOrEqualTo(2),
-            lessThanOrEqualTo(12)));
+        assertThat(DiceNotation.roll("2d6"), getExpectedValue(2, 12));
     }
 
     @Test
@@ -61,6 +55,11 @@ public class DiceTests {
 
     @Test
     public void testPercentileDice() {
-        assertThat(DiceNotation.roll("d%"), allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(100)));
+        assertThat(DiceNotation.roll("d%"), getExpectedValue(1, 100));
+    }
+
+    private Matcher<Integer> getExpectedValue(int min, int max) {
+        return allOf(greaterThanOrEqualTo(min),
+            lessThanOrEqualTo(max));
     }
 }
